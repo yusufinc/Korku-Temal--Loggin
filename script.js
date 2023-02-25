4f45e0647b4377d5f136b7312cf590f6ba76c519
yusufINC <75489069+yusufinc@users.noreply.github.com>
2023-02-25 09:09:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const loginButton = document.getElementById("login-button");
+const forgotPassword = document.getElementById("forgot-password");
+
+loginButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  // Burada giriş doğrulama kodu olacaktır.
+});
+
+forgotPassword.addEventListener("click", function(event) {
+  event.preventDefault
